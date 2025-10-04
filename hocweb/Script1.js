6195de2e1bd8ffd3ee12ea574025aae7d7b620ef
c230577 <c230577@ccg.ac.jp>
2025-10-04 15:45:41 +0900

    commit all

---
+++ hocweb/Script1.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    document.getElementById("text").style.color = "red";
+    document.getElementById("info").style.color = "red";
+
+}
+
+function showinfo(content) {
+    document.getelementbyid("info").innertext = content;
+}
